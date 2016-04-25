ca51f50b44dbe162f30dea2f4c43d0051cc7bd1f
kuramu1108 <colagarychen@gmail.com>
2016-04-25 11:52:47 +1000

    init
    update previously done source code

--- vagrant/project.py
+++ vagrant/project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 app = Flask(__name__)
 
 from sqlalchemy import create_engine
@@ -27,6 +27,7 @@ def newMenuItem(restaurant_id):
 		newItem = MenuItem(name=request.form['name'], restaurant_id=restaurant_id)
 		session.add(newItem)
 		session.commit()
+		flash("new menu item created")
 		return redirect(url_for('listMenuItems', restaurant_id=restaurant_id))
 	else:
 		return render_template('newmenuitem.html', restaurant_id=restaurant_id)
@@ -37,8 +38,15 @@ def editMenuItem(restaurant_id, menu_id):
 	if request.method == 'POST':
 		if request.form['name']:
 			item.name = request.form['name']
+		if request.form['description']:
+			item.description = request.form['description']
+		if request.form['price']:
+			item.price = request.form['price']
+		if request.form['course']:
+			item.course = request.form['course']
 		session.add(item)
 		session.commit()
+		flash("menu item editted")
 		return redirect(url_for('listMenuItems', restaurant_id=restaurant_id))
 	else:
 		return render_template('editmenuitem.html', item=item)
@@ -49,10 +57,12 @@ def deleteMenuItem(restaurant_id, menu_id):
 	if request.method == 'POST':
 		session.delete(item)
 		session.commit()
+		flash("menu item deleted")
 		return redirect(url_for('listMenuItems', restaurant_id=restaurant_id))
 	else:
 		return render_template('deletemenuitem.html', item=item)
 		
 if __name__ == '__main__':
+	app.secret_key = 'something'
 	app.debug = True
 	app.run(host = '0.0.0.0', port = 5000)
